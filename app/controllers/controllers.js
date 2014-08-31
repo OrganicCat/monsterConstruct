b767b800b9b4047b721f3d0485f5f2b565c5c215
OrganicCat <lee.delarm@gmail.com>
2014-08-30 20:11:46 -0500

    updated controller

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -14,6 +14,7 @@
       damage: 1
     },
     exp: 0,
+    gold: 0,
     getDamage: function() {
       return this.stats.strength + this.weapon.damage;
     }
@@ -27,8 +28,8 @@
     damage: 1,
     level: 1,
     reward: {
-      gold: 1,
-      exp: 1
+      gold: 10,
+      exp: 5
     }
   },
   {
@@ -43,22 +44,44 @@
     }
   }];
 
+  var weaponList = [
+  {
+    name: "Rusty Dagger",
+    damage: 2,
+    cost: 5
+  },
+  {
+    name: "Shiny Dagger",
+    damage: 10,
+    cost: 50
+  },
+  {
+    name: "Shortsword",
+    damage: 15,
+    cost: 100
+  }
+  ];
+
   app.controller('FightController', function() {
     var fight = this;
 
     fight.player = player;
     fight.monster = {};
     fight.monsterList = monsters;
+    fight.weaponList = weaponList;
+    fight.selectedWeapon = {};
     fight.result = [];
 
     fight.combat = function() {
       fight.monster.hp -= fight.player.getDamage();
 
+      fight.result.push(fight.player.name + " did " + fight.player.getDamage() + " damage.");
+
       if (fight.monster.hp > 0) {
         fight.player.hp -= fight.monster.damage;
-        fight.result.push(fight.player.name + " did " + fight.player.getDamage() + " damage.");
+        fight.result.push(fight.monster.name + " did " + fight.monster.damage + " damage.");
       } else {
-        fight.levelUp();
+        fight.rewards();
         fight.result.push(fight.monster.name + " lost the battle!");
       }
 
@@ -73,15 +96,29 @@
       fight.result = [];
     };
 
+    fight.rewards = function() {
+      fight.player.exp += fight.monster.reward.exp;
+      fight.levelUp();
+      fight.player.gold += fight.monster.reward.gold;
+    };
+
     fight.levelUp = function() {
       var expToLevel = fight.player.level * 10;
-      fight.player.exp += fight.monster.reward.exp;
+      
       if (fight.player.exp >= expToLevel) {
         fight.player.exp = 0;
         fight.player.level++;
-        fight.player.damage++;
+        fight.player.stats.strength++;
         fight.player.maxhp += 10;
         fight.player.hp = fight.player.maxhp;
+      }
+    };
+
+    // Move this into a buy/sell controller later
+    fight.buyWeapon = function() {
+      if (fight.player.gold >= fight.selectedWeapon.cost) {
+        fight.player.gold -= fight.selectedWeapon.cost;
+        fight.player.weapon = fight.selectedWeapon;
       }
     };
 
